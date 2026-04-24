6eb16068b53d7fad124ace053b2498adafadfa77
Jeee-maker <blazenow123@gmail.com>
2026-04-24 12:26:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function handleLogin() {
     const webAppUrl = "https://script.google.com/macros/s/AKfycbzglhWAKYJ-tk7517n1qHqKtbmLsGNR1LTd_WRO0EsKHDrmvCxQMt6P78u6krbhGtt99g/exec";
 
     if (user === "" || pass === "") {
-        alert("Nama dan Sandi tidak boleh kosong!");
+        alert("Username dan Password tidak boleh kosonggggggggggggggggggggggg. Coba isi dulu ya!");
         return;
     }
 
@@ -16,16 +16,23 @@ async function handleLogin() {
         const data = await response.json();
 
         if (data.result === "success") {
+            const formatRupiah = (angka) => {
+        return new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+            minimumFractionDigits: 2
+        }).format(angka);
+    };
             // 1. ISI DATA DULU (Hubungkan data dari Google ke ID HTML)
-            document.getElementById('info-terbayar').innerText = data.terbayar || "0";
-            document.getElementById('info-kurang').innerText = data.kurang || "0";
-            document.getElementById('info-seharusnya').innerText = data.seharusnya || "0";
-            
+            document.getElementById('info-terbayar').innerText = formatRupiah(Number(data.terbayar) || 0);
+    document.getElementById('info-kurang').innerText = formatRupiah(Number(data.kurang) || 0);
+    document.getElementById('info-seharusnya').innerText = formatRupiah(Number(data.seharusnya) || 0);
+
             // 2. BARU PINDAH HALAMAN
             document.getElementById('login-page').style.display = 'none';
             document.getElementById('dashboard-page').style.display = 'block';
         } else {
-            alert("Nama Murid atau Sandi salah!");
+            alert("Hmmmmmmmmmmmmmmmmmmmmmm, Kaya nya Username atau Password kamu salah deh. Coba ingat-ingat lagi ya!");
         }
     } catch (error) {
         console.error("Error detail:", error);
